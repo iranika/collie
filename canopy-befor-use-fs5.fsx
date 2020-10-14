b2b806fde99dff27054d0275d2ccd1273bacfc57
iranika <7370486+iranika@users.noreply.github.com>
2020-10-14 17:53:13 +0900

    Add: impl driver dir path matching.

--- canopy-befor-use-fs5.fsx
+++ canopy-befor-use-fs5.fsx
@@ -10,8 +10,9 @@ open canopy.reporters
 open System
 
 //TODO: Win/Mac/Linuxによってディレクトリが変わるようなので、その分岐処理を実装
-let driverDir = 
-    System.Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\.nuget\\packages\\selenium.webDriver.chromedriver\\85.0.4183.8700\\driver\\win32"
+let driverDir = match System.Environment.OSVersion.Platform with
+    | System.PlatformID.Win32NT -> System.Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\.nuget\\packages\\selenium.webDriver.chromedriver\\85.0.4183.8700\\driver\\win32"
+    | System.PlatformID.Unix -> System.Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\.nuget\\packages\\selenium.webDriver.chromedriver\\85.0.4183.8700\\driver\\mac64"
     // System.Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\.nuget\\packages\\selenium.webDriver.chromedriver\\85.0.4183.8700\\driver\\linux64"
     // System.Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\.nuget\\packages\\selenium.webDriver.chromedriver\\85.0.4183.8700\\driver\\mac64"
 
